34367e327e293f88e00625139225b9db338223cd
Thornton <thorfernbacher@gmail.com>
2020-12-22 03:21:42 +0000

    removing development values

--- build/custom.mk
+++ build/custom.mk
@@ -1,4 +1,4 @@
 # Include custome targets and environment variables here
-export MM_SERVICESETTINGS_SITEURL=http://localhost:8065
-export MM_ADMIN_USERNAME=test
-export MM_ADMIN_PASSWORD=password
+export MM_SERVICESETTINGS_SITEURL=
+export MM_ADMIN_USERNAME=
+export MM_ADMIN_PASSWORD=
